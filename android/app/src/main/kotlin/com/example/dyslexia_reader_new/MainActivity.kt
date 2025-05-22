b45281b5f7c56c1e28e45683f4b7a8f5b61b019c
K-SIDHARTH <23r21a6789@mlrit.ac.in>
2025-05-22 21:25:49 +0530

    Initial commit of dyslexia_reader_new

---
+++ android/app/src/main/kotlin/com/example/dyslexia_reader_new/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dyslexia_reader_new
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/login_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Login", style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              controller: passwordController,
+              obscureText: true,
+              decoration: InputDecoration(labelText: 'Password'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, '/home');
+              },
+              child: Text("Login"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
